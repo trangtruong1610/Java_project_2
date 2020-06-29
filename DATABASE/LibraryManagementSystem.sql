aa8450f096338a84db926533b36aaf3d9a9ba361
An Huynh <sora0aozora@gmail.com>
2020-06-29 20:01:08 +0700

    Database Updated_2020029
    1. Delete Table BookCategory
    2. Add column BookNo to Table Category
    3. Add column D_EstReturn to Table LendingHistory (them cot ngay tra du kien)
    4. Add Column YPublished to Table BookDetail (Nam xuat ban)
    5. Move ShelfNo from Table BookItem to Table BookDetail

--- DATABASE/LibraryManagementSystem.sql
+++ DATABASE/LibraryManagementSystem.sql
@@ -5,52 +5,48 @@ USE Circle_JavaProject2
 GO
 
 CREATE TABLE Notification (
-	NotiNo INT IDENTITY(1,1) PRIMARY KEY,
+	NotiNo INT IDENTITY PRIMARY KEY,
 	NotiSubject VARCHAR(255),
 	NotiContent VARCHAR(255) 
 )
 GO
 
+CREATE TABLE Shelf (
+	ShelfNo INT IDENTITY PRIMARY KEY,
+	Location VARCHAR(255) 
+)
+GO
+
 CREATE TABLE BookDetail (
-	BookNo INT IDENTITY(1,1) PRIMARY KEY,
+	BookNo INT IDENTITY PRIMARY KEY,
 	Title VARCHAR(255),
 	Publisher VARCHAR(255),
 	Author VARCHAR(100),
 	ISBN VARCHAR(13),
 	Pages INT,
+	ShelfNo INT REFERENCES Shelf (ShelfNo),
+	YPublished DATE,
 	Status BIT DEFAULT 1
 )
 GO
 
 CREATE TABLE Category (
-	CategoryNo INT IDENTITY(1,1) PRIMARY KEY,
+	CategoryNo INT IDENTITY PRIMARY KEY,
+	BookNo INT REFERENCES BookDetail(BookNo),
 	CategoryName VARCHAR(100)
 )
 GO
 
-CREATE TABLE Shelf (
-	ShelfNo INT IDENTITY(1,1) PRIMARY KEY,
-	Location VARCHAR(255) 
-)
-GO
-
 CREATE TABLE BookItem (
-	BookItemNo INT IDENTITY(1,1),
+	BookItemNo INT IDENTITY,
 	BookNo INT REFERENCES BookDetail (BookNo),
-	ShelfNo INT REFERENCES Shelf (ShelfNo),
 	BookID VARCHAR(10) PRIMARY KEY,
 	Status BIT DEFAULT 1
 )
 GO
 
-CREATE TABLE BookCategory (
-	BookNo INT PRIMARY KEY REFERENCES BookDetail (BookNo),
-	CategoryNo INT REFERENCES Category (CategoryNo)
-)
-GO
-
 CREATE TABLE Account (
-	AccountNo INT IDENTITY(1,1),
+	AccountNo INT IDENTITY,
 	AccountName VARCHAR(100),
 	LibraryID VARCHAR(10) PRIMARY KEY,
 	Password VARCHAR(20),
@@ -63,7 +59,7 @@ CREATE TABLE Account (
 GO
 
 CREATE TABLE ImageLink (
-	ImgNo INT IDENTITY(1,1) PRIMARY KEY,
+	ImgNo INT IDENTITY PRIMARY KEY,
 	BookNo INT REFERENCES BookDetail (BookNo),
 	Link VARCHAR(255),
 	IsCover BIT,
@@ -72,7 +68,7 @@ CREATE TABLE ImageLink (
 GO
 
 CREATE TABLE Reservation (
-	ReserveNo INT IDENTITY(1,1) PRIMARY KEY,
+	ReserveNo INT IDENTITY PRIMARY KEY,
 	LibraryID VARCHAR(10) REFERENCES Account (LibraryID),
 	BookID VARCHAR(10) REFERENCES BookItem (BookID),
 	DReserve DATE,
@@ -81,19 +77,62 @@ CREATE TABLE Reservation (
 GO
 
 CREATE TABLE LendingHistory (
-	LendNo INT IDENTITY(1,1) PRIMARY KEY,
+	LendNo INT IDENTITY PRIMARY KEY,
 	LibraryID VARCHAR(10) REFERENCES Account (LibraryID),
 	BookID VARCHAR(10) REFERENCES BookItem (BookID),
-	DIssued DATE,
+	DIssued DATE DEFAULT GETDATE(),
+	D_EstReturn DATE DEFAULT DATEADD(day, 30, GETDATE()),
 	DReturned DATE,
 	Status BIT DEFAULT 1
 )
 GO
 
 CREATE TABLE Fine (
-	FineNo INT IDENTITY(1,1) PRIMARY KEY,
+	FineNo INT IDENTITY PRIMARY KEY,
 	LendNo INT REFERENCES LendingHistory (LendNo),
 	OverduedDate INT,
 	FineAmount INT 
 )
+GO
+
+CREATE TRIGGER InsertNewBook ON BookDetail
+INSTEAD OF INSERT
+AS BEGIN
+	INSERT INTO BookDetail(Title, Publisher, Author, ISBN, Pages)
+	SELECT i.Title, i.Publisher, i.Author, i.ISBN, i.Pages
+	FROM inserted i
+	WHERE i.ISBN NOT IN (SELECT ISBN FROM BookDetail)
+END
+GO
+
+CREATE TRIGGER DeleteBook ON BookDetail
+INSTEAD OF DELETE
+AS BEGIN
+	UPDATE BookDetail
+	SET Status = 0;
+END
+GO
+
+CREATE PROC DisplayAllBook
+AS BEGIN
+	SELECT * FROM BookDetail
+END
+GO
+
+CREATE PROC DisplayBookCover (@BookNo int)
+AS BEGIN
+	SELECT * FROM ImageLink
+	WHERE BookNo = @BookNo AND IsCover = 1
+END
+GO
+
+CREATE TRIGGER FineBook ON Fine
+FOR INSERT
+AS BEGIN
+
+	INSERT INTO Fine(LendNo, OverduedDate, FineAmount)
+	SELECT LendNo, DATEDIFF(day, GETDATE(), DIssued), DATEDIFF(day, GETDATE(), D_EstReturn)*5
+	FROM LendingHistory
+	WHERE DATEDIFF(day, GETDATE(), DIssued) > 30 AND Status = 1 AND LendNo NOT IN (SELECT LendNo FROM Fine)
+END
 GO

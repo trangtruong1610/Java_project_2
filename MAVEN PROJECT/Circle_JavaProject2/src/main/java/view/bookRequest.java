30422150aa1d8bdeacdb1e2186fafb6ea05368b6
Trang Truong <trangtruon1610@gmail.com>
2020-07-27 19:56:15 +0700

    remove search

--- MAVEN PROJECT/Circle_JavaProject2/src/main/java/view/bookRequest.java
+++ MAVEN PROJECT/Circle_JavaProject2/src/main/java/view/bookRequest.java
@@ -38,7 +38,6 @@ import javax.swing.JTable;
 public class bookRequest extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField keyword;
 	private JButton btnLoadRequest;
 	private JScrollPane scrollPane;
 	private JTable table;
@@ -73,12 +72,6 @@ public class bookRequest extends JFrame {
 		lblBookRequest.setFont(new Font("Dialog", Font.BOLD, 20));
 		lblBookRequest.setHorizontalAlignment(SwingConstants.CENTER);
 		
-		keyword = new JTextField();
-		keyword.setColumns(10);
-		
-		JLabel lblSearch = new JLabel("Search");
-		lblSearch.setFont(new Font("Dialog", Font.BOLD, 16));
-		
 		JButton btnAccept = new JButton("Accept");
 		btnAccept.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -103,42 +96,34 @@ public class bookRequest extends JFrame {
 		scrollPane = new JScrollPane();
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(111)
-							.addComponent(btnAccept)
-							.addGap(56)
-							.addComponent(btnDenied))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(25)
-							.addComponent(lblSearch)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(keyword, GroupLayout.PREFERRED_SIZE, 198, GroupLayout.PREFERRED_SIZE)
-							.addGap(28)
-							.addComponent(btnLoadRequest, GroupLayout.PREFERRED_SIZE, 168, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(13, Short.MAX_VALUE))
+					.addGap(111)
+					.addComponent(btnAccept)
+					.addGap(56)
+					.addComponent(btnDenied)
+					.addContainerGap(258, Short.MAX_VALUE))
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap(257, Short.MAX_VALUE)
-					.addComponent(lblBookRequest, GroupLayout.PREFERRED_SIZE, 260, GroupLayout.PREFERRED_SIZE)
-					.addGap(239))
-				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
 					.addContainerGap()
 					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 735, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+					.addContainerGap(272, Short.MAX_VALUE)
+					.addComponent(btnLoadRequest, GroupLayout.PREFERRED_SIZE, 168, GroupLayout.PREFERRED_SIZE)
+					.addGap(316))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(225)
+					.addComponent(lblBookRequest, GroupLayout.PREFERRED_SIZE, 260, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(271, Short.MAX_VALUE))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addContainerGap()
 					.addComponent(lblBookRequest, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
-					.addGap(52)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblSearch)
-						.addComponent(keyword, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnLoadRequest, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE))
-					.addGap(47)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(btnLoadRequest, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE)
+					.addGap(87)
 					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 228, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED, 59, Short.MAX_VALUE)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
